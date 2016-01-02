8cab72c57300c9ff45ebff2ba1b658397a66fbe0
h-sato <hisaki.0128@hotmail.co.jp>
2016-01-03 01:34:00 +0900

    ちくわぶ対応

--- zoi-chan-bot.rb
+++ zoi-chan-bot.rb
@@ -14,17 +14,13 @@ module Idobata
 			to_body_plain = message.to_body_plain()
 			if /今日も一日/ =~ to_body_plain
 				to_say( message.room_id, message.sender_name, "がんばるぞい!" )
-			end
-			if /おはよう$/ =~ to_body_plain || /おはー$/ =~ to_body_plain || /おは$/ =~ to_body_plain
+			elsif /おはよう$/ =~ to_body_plain || /おはー$/ =~ to_body_plain || /おは$/ =~ to_body_plain
 				to_say( message.room_id, message.sender_name, "おはようございます！ 今日も一日がんばるぞい！");
-			end
-			if /こんにちは$/ =~ to_body_plain || /こにちはー$/ =~ to_body_plain || /こん$/ =~ to_body_plain
+			elsif /こんにちは$/ =~ to_body_plain || /こにちはー$/ =~ to_body_plain || /こん$/ =~ to_body_plain
 				to_say( message.room_id, message.sender_name, "こんにちは！ 午後もがんばってください！")
-			end
-			if /こんばんは$/ =~ to_body_plain || /ばんー$/ =~ to_body_plain || /ばん$/ =~ to_body_plain
+			elsif /こんばんは$/ =~ to_body_plain || /ばんー$/ =~ to_body_plain || /ばん$/ =~ to_body_plain
 				to_say( message.room_id, message.sender_name, "こんばんは！ 無理をしすぎないようにしてください！")
-			end
-			if /(.*)？/ =~ to_body_plain || /(.*)\?/ =~ to_body_plain
+			elsif /(.*)[？|?|！|!]/ =~ to_body_plain 
 				question = $1
 				if /(.*)って何$/ =~ question || /(.*)って$/ =~ question
 					question = $1
@@ -37,30 +33,31 @@ module Idobata
 				end
 				url = get_image_url(question)
 				if !url.nil?
-					to_say( message.room_id, message.sender_name, question + " はこちらでしょうか! " + url)
+					to_say( message.room_id, message.sender_name, question + " ! " + url)
 				else
 					to_say( message.room_id, message.sender_name, "ごめんなさい. わからないです・・。")
 				end
-			end
-
-			if /LGTM/ =~ to_body_plain || /lgtm/ =~ to_body_plain
+			elsif /LGTM/ =~ to_body_plain || /lgtm/ =~ to_body_plain
 				url = get_lgtm_url()
-				to_say( message.room_id, message.sender_name, "良いと思います！ " + url )
+				to_say( message.room_id, message.sender_name, "LGTM！ " + url )
 			end
 		end
 
 		def get_image_url(search)
-			params = URI.encode("q=#{search}&lr=lang_ja&ie=utf-8&oe=utf-8&aq=t&rls=org.mozilla:ja:official&client=firefox-a&v=1.0&rsz=large&start=#{rand(8)}");
+			params = URI.encode("q=#{search}");
 			response = Net::HTTP.get(
-				'ajax.googleapis.com',
-				'/ajax/services/search/images?'+params
+				'api.tiqav.com',
+				'/search.json?'+params
 			)
-			responseJson = JSON.parse(response)
-			if responseJson['responseData'].nil?
+			responseJsons = JSON.parse(response)
+			if responseJsons.nil?
 				return nil
 			end
-			responseResults = responseJson['responseData']['results']
-			responseResults.at( rand(8) )['url']
+			random_index = rand(responseJsons.length)
+			if responseJsons[random_index]["id"].nil?
+				return nil
+			end
+			"http://tiqav.com/"+responseJsons[random_index]["id"]+"."+responseJsons[random_index]["ext"]
 		end
 
 		def get_lgtm_url()
